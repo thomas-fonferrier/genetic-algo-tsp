ba2f5f7f8eac3095ca6fbde994a00be542b4d905
Thomas <thomasfonferrier@gmail.com>
2026-03-27 17:08:55 +0100

    tiny et gitignore

--- tp6_tiny.py
+++ tp6_tiny.py
@@ -1,5 +1,12 @@
-import requests
+import Config
 
+student_id = "Thomas & Loris"
+instance_id = "regions"
+
+data = Config.fetch_data(instance_id)
+result = Config.main(instances=data["cities"], selection_method="roulette", mutation_method="permutation", n_loop=100, n_individus=100)
+response = Config.upload_result(student_id=student_id, instance_id=instance_id, initial_instances=data["cities"], result=result)
+print(response)
 
 
 
