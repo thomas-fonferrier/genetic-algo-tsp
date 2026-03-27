d807223923dac015fe2859faae7ab3358f750b65
Thomas <thomasfonferrier@gmail.com>
2026-03-27 17:21:40 +0100

    debug final

--- tp6_tiny.py
+++ tp6_tiny.py
@@ -4,7 +4,7 @@ student_id = "Thomas & Loris"
 instance_id = "regions"
 
 data = Config.fetch_data(instance_id)
-result = Config.main(instances=data["cities"], selection_method="roulette", mutation_method="permutation", n_loop=100, n_individus=100)
+result = Config.main(instances=data["cities"], selection_method="elitisme", mutation_method="permutation", n_loop=1000, n_individus=1000, n_perm=3)
 response = Config.upload_result(student_id=student_id, instance_id=instance_id, initial_instances=data["cities"], result=result)
 print(response)
 
