1620ce08275b14b7087dd92c840b9637ec1ee15f
MAZUEL Loris <mazuelloris@orange.fr>
2026-03-27 15:28:21 +0100

    roulette + elitisme

--- Config.py
+++ Config.py
@@ -21,7 +21,7 @@ def distance_Haversine(A,B):
     a=np.sin(np.abs(A[1]-B[1])/2)**2 + np.cos(A[1]) * np.cos(B[1]) * np.sin(np.abs(A[0]-B[0])/2)
     return 2 * R * np.arcsin(np.sqrt(a))
 
-def fitness(trip:list): # Distance du parcous
+def fitness(trip:list): # Distance du parcouru
     distance=0
     for i in range(len(trip)-1):
         distance += distance_euclidienne(trip[i], trip[i+1])
@@ -38,3 +38,34 @@ def init(population:list, nb_slt:int):
             list_indice.pop(indice)
         solutions.append(individu)
     return solutions
+
+## Fonctions de séléction :
+
+def roulette(population_fit:list):
+    n=len(population_fit)
+    selection = []
+    sum_fit=0
+    for k in range(n):
+        sum_fit+=population_fit[k][0]
+    for k in range(n):
+        proba=population_fit[k][0]/sum_fit
+        if rd.randint(0,100) < proba:
+            selection.append(population_fit[k])
+    return selection
+
+def tri_pivot(l:list,p):
+    if len(l)==1:
+        return l
+    l1=[]
+    l2=[]
+    for k in range(1,len(l1)):
+        if l[k][0]<p:
+            l1.append(l[k])
+        else :
+            l2.append(l[k])
+    return tri_pivot(l1, l1[0][0]) + p + tri_pivot(l2, l2[0][0])
+
+def elitisme(population_fit:list, n:int):
+    pop_triee=tri_pivot(population_fit,population_fit[0][0])
+    return pop_triee[:n]
+        
